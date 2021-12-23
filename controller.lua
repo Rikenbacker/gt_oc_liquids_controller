e6fe4fb3867b238c5b246deec030f6848a0805e4
Rikenbacker <Rikenbacker@yandex.ru>
2021-12-23 22:17:23 +0300

    Update controller.lua

--- controller.lua
+++ controller.lua
@@ -17,11 +17,21 @@ local function waitForWork(args)
   return 0
 end
 
+local function sendIngridients(args)
+  rs.setBundledOutput(2, 14, 15)
+  
+  local chestInv = trs.getAllStacks(1)
+
+  
+  return 1
+end
+
 while true do
   if mode == 0 then
     mode = waitForWork()
   end  
   if mode == 1 then
+    mode = sendIngridients()
   end
   if mode == 2 then
   end
