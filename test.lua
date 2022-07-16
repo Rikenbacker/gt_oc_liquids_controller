bcc4759a93f39f30a433f88bbdd51bb774988ec1
Rikenbacker <Rikenbacker@yandex.ru>
2022-07-16 22:25:32 +0300

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,5 @@
+local component = require("component")
+local trs = component.transposer
+print(trs)
+items = trs.getALlStacks(2)
+for i, item in pairs(items.getAll()) fo if item.name and i < 30 then print (i, item.name, item.damage, item.size. item) end end
