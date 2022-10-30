097d6921736ce6bb60d8efbee4316af142ad0cd1
Rikenbacker <Rikenbacker@yandex.ru>
2022-10-30 16:55:19 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,5 +2,6 @@ local component = require("component")
 trs = component.transposer
 print(trs)
 print(trs.getAllStacks(2))
+print(trs.getInventoryName(2))
 items = trs.getAllStacks(2)
 for i, item in pairs(items.getAll()) do if item.name and i < 30 then print (i, item.name, item.damage, item.size, item) end end
